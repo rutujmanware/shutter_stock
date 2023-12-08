1f787d137d010d75f33b20287b1e02389dfa60f2
Rituj Manware <manwarerutuj@gmail.com>
2023-12-08 09:09:50 +0530

    search text area button fixed.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 // import SearchIcon from '@mui/icons-material/Search';
+import { FaSearch } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className="relative">
-      <img
-        src="/pexels-pixabay-206359.jpg"
-        alt="bg-image"
-        className="w-full "
-      />
-      <div className="absolute inset-0 flex flex-col items-center justify-center ">
+      <img src="/pexels-pixabay-206359.jpg" alt="bg-image" className="w-full" />
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
         <h1 className="text-white text-5xl z-10 p-5 justify-center text-center mt-20">
           Unleash your creativity with unrivaled images
         </h1>
@@ -17,25 +14,36 @@ const Hero = () => {
           and editorial images.
         </p>
         <div className="w-full flex px-7 sm:px-10 md:px-32 lg:px-56">
-          <div className=" flex flex-row items-center mt-5 backdrop-blur-md p-5 w-full">
+          <div className=" flex flex-row items-center mt-5 backdrop-blur-md p-5 w-full rounded-md justify-between">
             <select
               id="filter"
               name="filter"
-              className="mr-3 p-5 border border-gray-300 rounded-md"
+              className="mr-3 p-4 border border-gray-300 rounded-md shadow-sm"
             >
               <option value="all">All images</option>
               <option value="marked">Editorial</option>
               <option value="not-marked">Video</option>
             </select>
-            <input
-              type="text"
-              placeholder="Search"
-              className="w-full p-5 border border-gray-300 rounded-l"
-            />
-            <button className="bg-red-500 p-5 rounded-r"> s</button>
-            <button className="ml-5 bg-white px-10 rounded-sm py-2">
-              S by image
+            <div className="flex w-3/5">
+              <div className="w-5/6">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="w-full p-4 rounded-l shadow-sm outline-none"
+                />
+              </div>
+              <div className="w-1/6">
+                <button className="bg-red-500 p-5 rounded-r shadow-sm outline-none ">
+                  <FaSearch />
+                </button>
+              </div>
+            </div>
+            <div className="hidden md:flex ml-3">
+            <button className=" bg-white px-9 rounded-md p-4 shadow-md">
+              image
             </button>
+
+            </div>
           </div>
         </div>
       </div>
